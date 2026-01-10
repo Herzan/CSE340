6d63f65c2b5f50b35c0dbd84e7076a3736038b88
Herzan <46695573+Herzan@users.noreply.github.com>
2026-01-10 14:21:38 -0600

    c

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const router = express.Router();
+/* ****************************
+ * Static Routes
+ **************************** */
+const express = require("express")
+const router = new express.Router()
 
-// Static Routes
-// Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+/* ****************************
+ * Home route
+ **************************** */
+router.get("/", (req, res) => {
+  res.render("index", {
+    title: "Home"
+  })
+})
 
-module.exports = router;
-
-
-
+module.exports = router

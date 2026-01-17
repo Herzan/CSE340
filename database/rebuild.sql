e3be29c8f969d38e0185a6aed0fe0b3b91c80b71
Herzan <46695573+Herzan@users.noreply.github.com>
2026-01-17 15:56:51 -0600

    h

---
+++ database/rebuild.sql
@@ -0,0 +1,11 @@
+-- Update GM Hummer description
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM'
+AND inv_model = 'Hummer';
+
+-- Update image paths
+UPDATE inventory
+SET
+  inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+  inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
